fdc29197965602fc245dae626b6c8523cd2f3f03
Alivki <iverlindholm@icloud.com>
2024-10-25 14:16:56 +0200

    intilized userInterface class in main, called init and start methods

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -10,6 +10,9 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO: implement main method logic
+    TUI ui = new TUI();
+    ui.init();
+    ui.start();
 
     Grocery ost = new Grocery("cheese", "kg", 100f, "10.11.2024", 1.2f);
 
